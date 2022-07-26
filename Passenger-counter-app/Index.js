bd1e35ccb9cb52f600a0bba838f1751bcf7aa211
Aditya <adityachatap123@gmail.com>
2022-07-26 22:40:54 +0530

    Finished Project

--- Passenger-counter-app/Index.js
+++ Passenger-counter-app/Index.js
@@ -1,4 +1,5 @@
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 let count = 0
 
 function increment() {
@@ -7,10 +8,14 @@ function increment() {
 }
 
 function save(){
-    console.log(count)
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
 
-save()
+
+
 
 
 
